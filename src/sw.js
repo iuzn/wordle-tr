393a845bc570d0502d733d43cc4b1b8486d2c70c
Ibrahim Uzun <iuzun@marun.edu.tr>
2022-02-16 10:54:12 +0300

    Update PWA

---
+++ src/sw.js
@@ -0,0 +1,4 @@
+// sw.js
+import { precacheAndRoute } from 'workbox-precaching'
+// self.__WB_MANIFEST is default injection point
+precacheAndRoute(self.__WB_MANIFEST)
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { VitePWA } from "vite-plugin-pwa"
 // https://vitejs.dev/config/
-import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-  plugins: [
-    VitePWA(),
-    vue({
-      reactivityTransform: true
-    })
-  ]
+  plugins: [vue({
+    reactivityTransform: true
+  }), VitePWA({ strategies: 'injectManifest',
+    manifest: {
+      // content of manifest
+    },
+    injectManifest: {
+      // workbox options for injectManifest
+    }}),],
 })
