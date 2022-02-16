80bb55a0cea8ecf5f5b93853c8a6652f7a44966f
Ibrahim Uzun <iuzun@marun.edu.tr>
2022-02-17 02:03:01 +0300

    enable PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,25 @@ import { VitePWA } from "vite-plugin-pwa"
 export default defineConfig({
   plugins: [vue({
     reactivityTransform: true
-  }), VitePWA(),],
+  }),     VitePWA({
+    strategies: 'generateSW',
+    manifest: {
+      name: 'Wordle Kapışması',
+      short_name: 'Wordle TR',
+      background_color: '#FFFFFF',
+      theme_color: '#FFFFFF',
+      icons: [
+        {
+          src: '/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+  }),],
 })
